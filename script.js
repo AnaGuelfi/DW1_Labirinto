14dca993246d75204da9763a8397e7eae2d4ec17
AnaGuelfi <aguelfie@gmail.com>
2023-04-13 11:01:52 -0300

    Modularização do Código Original

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 (function(){
+    // definindo o canvas
     var cnv = document.querySelector("canvas");
     var ctx = cnv.getContext("2d");
+    // dimensões
+    var WIDTH = cnv.width, HEIGHT = cnv.height;
     // pixel do canvas
     var tileSize = 64;
-    // pixel da imagem
-    var tileSrcSize = 96;
+
     // imagem
     var img = new Image();
     img.src = "img/img.png";
+    // pixel da imagem
+     var tileSrcSize = 96;
     // jogo renderiza apenas com a imagem carregada
     img.addEventListener("load", function(){
         requestAnimationFrame(loop, cnv); // responsável pela primeira chamada à função loop
     }, false);
-    // dimensões do canvas
-    var WIDTH = cnv.width, HEIGHT = cnv.height;
 
     // personagem
     var player = {
@@ -30,14 +32,14 @@
     var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40; // códigos das setas do teclado
     var mvLeft = false, mvUp = false, mvRight = false, mvDown = false; // personagem começa parado
 
-    // caminho do labirinto
+    // labirinto
     var maze = [
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -53,30 +55,11 @@
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
     ];
-
     // tamanho total do layout do labirinto
     var T_WIDTH = maze[0].length * tileSize,
         T_HEIGHT = maze.length * tileSize;
-
     // paredes para colisão
     var walls = [];
-
-    // tornar as paredes sensíveis à colisão
-    for(var row in maze){
-        for(var column in maze[row]){
-            var tile = maze[row][column];
-            if(tile === 1){
-                // paredes de colisão
-                var wall = {
-					x: tileSize*column,
-					y: tileSize*row,
-					width: tileSize,
-					height: tileSize
-				};
-                walls.push(wall);
-            }
-        }
-    }
 
     // câmera
     var cam = {
@@ -99,7 +82,104 @@
         },
     };
 
-    function blockRectangle(objA,objB){
+    // esperar teclas de movimentação
+    window.addEventListener("keydown", keydownHandler, false); 
+    window.addEventListener("keyup", keyupHandler, false);
+
+    // tornar as paredes sensíveis à colisão
+    function makeWalls(){
+        for(var row in maze){
+            for(var column in maze[row]){
+                var tile = maze[row][column];
+                if(tile === 1){
+                    // paredes de colisão
+                    var wall = {
+                        x: tileSize*column,
+                        y: tileSize*row,
+                        width: tileSize,
+                        height: tileSize
+                    };
+                    walls.push(wall);
+                }
+            }
+        }
+    }
+
+    // personagem se move
+    function keydownHandler(e){
+        var key = e.keyCode;
+        switch(key){
+            case LEFT:
+                mvLeft = true;
+                break;
+            case RIGHT:
+                mvRight = true;
+                break;
+            case UP:
+                mvUp = true;
+                break;
+            case DOWN:
+                mvDown = true;
+                break;
+        }
+    }
+
+    // personagem para de se mover
+    function keyupHandler(e){ 
+        var key = e.keyCode;
+        switch(key){
+            case LEFT:
+                mvLeft = false;
+                break;
+            case RIGHT:
+                mvRight = false;
+                break;
+            case UP:
+                mvUp = false;
+                break;
+            case DOWN:
+                mvDown = false;
+                break;
+        }
+    }
+
+    // movimentação constante do personagem
+    function movePlayer(){
+        if(mvLeft && !mvRight){ // se move para a esquerda
+            player.x -= player.speed;
+            player.srcY = tileSrcSize + player.height * 2;
+        } else if(mvRight && !mvLeft){ // se move para a direita
+            player.x += player.speed;
+            player.srcY = tileSrcSize + player.height * 3;
+        }
+
+        if(mvUp && !mvDown){ // se move para cima
+            player.y -= player.speed;
+            player.srcY = tileSrcSize + player.height * 1;
+        } else if(mvDown && !mvUp){ // se move para baixo
+            player.y += player.speed;
+            player.srcY = tileSrcSize + player.height * 0;
+        }
+    }
+
+    // sensação de movimento enquanto o personagem se move
+    function feelMovements(){
+        if(mvLeft || mvRight || mvUp || mvDown){
+            player.countAnim++;
+
+            if(player.countAnim >= 40){
+                player.countAnim = 0;
+            }
+
+            player.srcX = Math.floor(player.countAnim / 5) * player.width;
+        } else{
+            player.srcX = 0;
+            player.countAnim = 0;
+        }
+    }
+
+    // cálculos do personagem em relação à parede
+    function checkPlayerWall(objA,objB){
         // distância X entre o personagem e a parede
 		var distX = (objA.x + objA.width/2) - (objB.x + objB.width/2);
         // distância Y entre o personagem e a parede
@@ -120,85 +200,16 @@
 		}
 	}
 
-    // esperar teclas de movimentação
-    window.addEventListener("keydown", keydownHandler, false); 
-    window.addEventListener("keyup", keyupHandler, false);
-
-    function keydownHandler(e){ // personagem se move
-        var key = e.keyCode;
-        switch(key){
-            case LEFT:
-                mvLeft = true;
-                break;
-            case RIGHT:
-                mvRight = true;
-                break;
-            case UP:
-                mvUp = true;
-                break;
-            case DOWN:
-                mvDown = true;
-                break;
-        }
-    }
-
-    function keyupHandler(e){ // personagem para de se mover
-        var key = e.keyCode;
-        switch(key){
-            case LEFT:
-                mvLeft = false;
-                break;
-            case RIGHT:
-                mvRight = false;
-                break;
-            case UP:
-                mvUp = false;
-                break;
-            case DOWN:
-                mvDown = false;
-                break;
-        }
-    }
-
-    function update(){ // atualizar elementos do jogo
-        // movimentação
-        if(mvLeft && !mvRight){ // se move para a esquerda
-            player.x -= player.speed;
-            player.srcY = tileSrcSize + player.height * 2;
-        } else if(mvRight && !mvLeft){ // se move para a direita
-            player.x += player.speed;
-            player.srcY = tileSrcSize + player.height * 3;
-        }
-
-        if(mvUp && !mvDown){ // se move para cima
-            player.y -= player.speed;
-            player.srcY = tileSrcSize + player.height * 1;
-        } else if(mvDown && !mvUp){ // se move para baixo
-            player.y += player.speed;
-            player.srcY = tileSrcSize + player.height * 0;
-        }
-
-        // sensação de movimento 
-        if(mvLeft || mvRight || mvUp || mvDown){
-            player.countAnim++;
-
-            if(player.countAnim >= 40){
-                player.countAnim = 0;
-            }
-
-            player.srcX = Math.floor(player.countAnim / 5) * player.width;
-        } else{
-            player.srcX = 0;
-            player.countAnim = 0;
-        }
-
-        // verificar se o personagem colidiu com alguma parede
+    // verificar se o personagem colidiu com alguma parede
+    function checkCollision(){
         for(var i in walls){
             var wall = walls[i];
-            blockRectangle(player, wall);
+            checkPlayerWall(player, wall);
         }
+    }
 
-        // acompanhamento da câmera em função da posição do personagem
+    // acompanhamento da câmera em função da posição do personagem
+    function moveCam(){
         if(player.x < cam.innerLeftBoundary()){
             cam.x = player.x - (cam.width * 0.25);
         }
@@ -211,23 +222,36 @@
         if(player.y + player.height > cam.innerBottomBoundary){
             cam.y = player.y + player.height - (cam.height * 0.75);
         }
+    }
 
-        // ajuste nas coordenadas da câmera para se enquadrarem no layout total do labirinto
+    // ajuste nas coordenadas da câmera para se enquadrarem no layout total do labirinto
+    function updateCam(){
         cam.x = Math.max(0, Math.min(T_WIDTH - cam.width, cam.x));
         cam.y = Math.max(0, Math.min(T_HEIGHT - cam.height, cam.y));
     }
 
-    function render(){ // representar graficamente os elementos do jogo
+    // atualizar elementos do jogo
+    function update(){ 
+        movePlayer();
+        feelMovements();
+        checkCollision();
+        moveCam();
+        updateCam();
+    }
+
+    // representar graficamente os elementos do jogo
+    function render(){ 
         ctx.clearRect(0,0,WIDTH,HEIGHT);
         ctx.save();
         ctx.translate(-cam.x, -cam.y);
         for(var row in maze){
             for(var column in maze[row]){
                 var tile = maze[row][column];
+
                 // coordenadas
                 var x = column*tileSize; 
                 var y = row*tileSize;
-                //ctx.fillRect(x, y, tileSize, tileSize); // desenhar retângulo: coordenadas; dimensões
+
                 ctx.drawImage(
                     img,
                     tile * tileSrcSize,0,tileSrcSize,tileSrcSize,
@@ -236,8 +260,6 @@
             }
         }
 
-        //ctx.fillStyle="#00f";
-        //ctx.fillRect(player.x, player.y, player.width, player.height);
         ctx.drawImage(
             img,
             player.srcX, player.srcY, player.width, player.height,
@@ -246,7 +268,9 @@
         ctx.restore();
     }
 
-    function loop(){ // repete as demais funções de modo recursivo
+    // repete as demais funções de modo recursivo
+    function loop(){ 
+        makeWalls();
         update();
         render();
         requestAnimationFrame(loop, cnv);
